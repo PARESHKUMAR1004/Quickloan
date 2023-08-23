107970c680e7cd0049571433a0837b4e05e23e9b
ajibeshMohapatra <ajibeshmohapatra@gmail.com>
2023-08-23 12:13:33 +0530

    Restrict Routes

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,13 +1,22 @@
 import React,{ useContext} from 'react'
 import { AuthContext } from '../service/AuthContext';
+import { Navigate } from 'react-router-dom';
 
 export default function Items () {
-    const { isLoading, user } = useContext(AuthContext);
+    const { isLoading,isUserAuthenticated, user } = useContext(AuthContext);
 
-    return (
+    const ItemComp = (data) => (
         <div>
-            <h1>Hi {isLoading ? "" : user ? user.email : ""}</h1>
+            <h1>Hi {data.email}</h1>
             <h1>Items List</h1>
         </div>
-    );
+    )
+
+    if(isLoading){
+        return(<div>Loading</div>)
+    } else if(isUserAuthenticated) {
+        return (<ItemComp data={user}></ItemComp>)
+    } else {
+        return <Navigate to='/login' />
+    }
 }
