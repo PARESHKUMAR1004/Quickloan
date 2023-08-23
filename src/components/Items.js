d5cf8926d6ae84734310f46fc074618dd5b31139
ajibeshMohapatra <ajibeshmohapatra@gmail.com>
2023-08-23 18:47:07 +0530

    Profile page solved

--- src/components/Items.js
+++ src/components/Items.js
@@ -5,17 +5,13 @@ import { Navigate } from 'react-router-dom';
 export default function Items () {
     const { isLoading,isUserAuthenticated, user } = useContext(AuthContext);
 
-    const ItemComp = (data) => (
-        <div>
-            <h1>Hi {data.email}</h1>
-            <h1>Items List</h1>
-        </div>
-    )
-
     if(isLoading){
         return(<div>Loading</div>)
     } else if(isUserAuthenticated) {
-        return (<ItemComp data={user}></ItemComp>)
+        return (<div>
+            <h1>Hi {user.email}</h1>
+            <h1>Items List</h1>
+        </div>)
     } else {
         return <Navigate to='/login' />
     }
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,22 +1,14 @@
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography'; 
 import React from "react";
-import { Container, CssBaseline, Grid } from '@mui/material';
+import { Container, CssBaseline, Divider, Grid, List, ListItem } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Edit } from '@mui/icons-material';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-
 
 const ProfilePage =()=>{
 
@@ -50,38 +42,35 @@ const ProfilePage =()=>{
       );
       
         const list = (
-            <Grid container spacing={2} sx={{marginTop: 2, justifyContent:"flex-start"}} >
+            <CardContent sx={{marginTop:2}}>
+                <List>
+                    <ListItem>
+                    <Grid container spacing={2} >
                     <Grid item xs={6} >
                         <Box> Full Name</Box>
                     </Grid>
                     <Grid item xs={6}>
                         <Box>Rod Johnson</Box>
                     </Grid>
-                <Grid item xs={6}>
+                    </Grid>
+                    </ListItem>
+                    <Divider light />
+                    <ListItem> 
+                    <Grid container spacing={2} >
+                    <Grid item xs={6}>
                         <Box>Employee Id</Box>
                     </Grid>
                     <Grid item xs={6}>
                         <Box>2080256</Box>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Box>Department</Box>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Box>CLT</Box>
-                    </Grid> 
-                    <Grid item xs={6}>
-                        <Box>Email</Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Box>bla bla bla</Box>
-                    </Grid> 
-                    <Grid item xs={6}>
-                        <Box>Phone Number</Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Box>0000000000</Box>
-                    </Grid>
-                </Grid>            
+                    </ListItem>
+                    <Divider light />
+                </List>
+            
+            
+            
+            </CardContent>
         );
        
     return(
@@ -106,7 +95,6 @@ const ProfilePage =()=>{
             </Grid>
 
         </Grid>
-        
       </Container>
     );
 }
