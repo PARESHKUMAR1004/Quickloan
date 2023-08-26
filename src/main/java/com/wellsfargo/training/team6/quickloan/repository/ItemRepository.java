a81cc518a17695075761ba3effe69fbbf7a61cb3
siddhartha2001 <45118607+siddhartha2001@users.noreply.github.com>
2023-08-26 07:51:30 +0530

    Update ItemRepository.java to only show not issued items

--- src/main/java/com/wellsfargo/training/team6/quickloan/repository/ItemRepository.java
+++ src/main/java/com/wellsfargo/training/team6/quickloan/repository/ItemRepository.java
@@ -10,18 +10,18 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ItemRepository extends JpaRepository<Item, Long>{
 
-	@Query("SELECT DISTINCT i.itemCategory FROM Item i")
+	@Query("SELECT DISTINCT i.itemCategory FROM Item i WHERE i.issueStatus = 'N'")
 	public List<String> getUniqueItemCategory();
 	
-	@Query("SELECT DISTINCT i.itemMake FROM Item i WHERE i.itemCategory = ?1")
+	@Query("SELECT DISTINCT i.itemMake FROM Item i WHERE i.issueStatus = 'N' AND i.itemCategory = ?1")
 	public List<String> getItemMakeByCategory(String itemCategory);
 	
-	@Query("Select DISTINCT i.itemDescription FROM Item i WHERE i.itemCategory = ?1 AND i.itemMake = ?2")
+	@Query("Select DISTINCT i.itemDescription FROM Item i WHERE i.issueStatus = 'N' AND i.itemCategory = ?1 AND i.itemMake = ?2")
 	public List<String> getItemDescByCategoryMake(String itemCategory, String itemMake);
 	
-	@Query("Select DISTINCT i.itemValuation FROM Item i WHERE i.itemCategory = ?1 AND i.itemMake = ?2 AND i.itemDescription = ?3")
+	@Query("Select DISTINCT i.itemValuation FROM Item i WHERE i.issueStatus = 'N' AND i.itemCategory = ?1 AND i.itemMake = ?2 AND i.itemDescription = ?3")
 	public List<Integer> getItemValuesByFilters(String itemCategory, String itemMake, String itemDescription);
 
-	@Query("Select i.itemId FROM Item i WHERE i.itemCategory = ?1 AND i.itemMake = ?2 AND i.itemDescription = ?3 AND i.itemValuation = ?4")
+	@Query("Select i.itemId FROM Item i WHERE i.issueStatus = 'N' AND i.itemCategory = ?1 AND i.itemMake = ?2 AND i.itemDescription = ?3 AND i.itemValuation = ?4")
 	public List<Long> getIdsByFilters(String itemCategory, String itemMake, String itemDescription, int itemValuation);
 }
